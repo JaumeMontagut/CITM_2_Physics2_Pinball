42a5c1469b133ec5114213ea99a34d8da5697d82
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-10-23 19:13:03 +0200

    adding fonts

--- Pinball-Code/ModuleFonts.cpp
+++ Pinball-Code/ModuleFonts.cpp
@@ -1,7 +1,10 @@
+#include "Application.h"
 #include "ModuleFonts.h"
-
 #include "SDL2_ttf-2.0.14/include/SDL_ttf.h"
 #pragma comment(lib, "SDL2_ttf-2.0.14/lib/x86/SDL2_ttf.lib")
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include <string>
 ModuleFonts::ModuleFonts(bool start_enabled)
 {
 }
@@ -19,6 +22,41 @@ bool ModuleFonts::Init()
 	}
 	return true;
 }
+
+SDL_Texture * ModuleFonts::CreateTextTexture(TTF_Font * fonts, char * text)
+{
+	SDL_Surface* surface = TTF_RenderText_Solid(fonts, text, {0,0,0});
+	if (surface == NULL)
+	{
+		LOG("Text Render Error &s\n", TTF_GetError());
+		return nullptr;
+	}
+	
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(App->renderer->renderer,surface);
+	if (tex == NULL)
+	{
+		LOG("Text Texture Creation Error %s \n", SDL_GetError());
+		return nullptr;
+	}
+	SDL_FreeSurface(surface);
+
+	return tex;
+}
+
+SDL_Texture * ModuleFonts::GetText(char * text, char * filename, int size)
+{
+	return nullptr;
+}
+
+TTF_Font * ModuleFonts::GetFont(std::string filename, int size)
+{
+	std::string  fullPath = SDL_GetBasePath();
+	fullPath.append("sprites/fonts" + filename);
+	
+	return nullptr;
+}
+
+
 
 bool ModuleFonts::CleanUp()
 {
--- Pinball-Code/ModuleFonts.h
+++ Pinball-Code/ModuleFonts.h
@@ -4,13 +4,22 @@
 
 #include "Module.h"
 
+typedef struct _TTF_Font TTF_Font;
+struct SDL_Texture;
 
 class ModuleFonts : public Module
 {
 public:
+	p2List<SDL_Texture*> textureFonts;
+	p2List<TTF_Font*> mFonts;
+public:
 	ModuleFonts(bool start_enabled = true);
 	~ModuleFonts();
 	bool Init()  override;
+	SDL_Texture* CreateTextTexture(TTF_Font* fonts , char* text);
+	SDL_Texture* GetText(char* text, char* filename, int size);
+	TTF_Font* GetFont(std::string filename, int size);
+
 	bool CleanUp() override;
 };
 
